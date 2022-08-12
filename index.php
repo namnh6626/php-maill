dedb64aba05aa3f5b236e108c1111f9badb92e5f
Nam Ngo <45323376+namnh6626@users.noreply.github.com>
2022-08-12 14:38:02 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,32 +1,149 @@
+
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<html style="font-size: 16px;" lang="en"><head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>PHP send mail</title>
-</head>
-<body>
-<div class="container">
-    <h2>Contact Form</h2>
-       
-    <form action="sendmail.php" method="POST">
-        <label for="firstName">First Name</label>
-        <input type="text" name="firstName" placeholder="First name" required>
- 
-        <label for="lastName">Last Name</label>
-        <input type="text" name="lastName" placeholder="Nhập họ và tên...lastName" required>
-
-        <label for="email">Email</label>
-        <input type="text"  name="email" placeholder="Email" required>
- 
-        <label for="message">Message</label>
-        <textarea  name="message" cols="50" rows="10" placeholder="Message" required></textarea>
- 
-        <input type="submit" value="Submit">
-    </form>
-</div>
-</body>
-</html>
+    <meta charset="utf-8">
+    <meta name="keywords" content="​Lorem ipsum dolor.">
+    <meta name="description" content="">
+    <title>Home</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Home.css" media="screen">
+    <!-- <script class="u-script" type="text/javascript" src="jquery.js" "="" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" "="" defer=""></script> -->
+    <meta name="generator" content="Nicepage 4.16.0, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/1e3166b9898f51feba8702fd180b549434b7598b716b877f747b3b26215b4c002a8f22ff3e392a0ff25685aa57654ff0d12cbdb8806a4261d8f263_1280.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+  </head>
+  <body data-home-page="Home.html" data-home-page-title="Home" class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-header" id="sec-6146"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="#" class="u-image u-logo u-image-1" data-image-width="1280" data-image-height="1029">
+          <img src="images/1e3166b9898f51feba8702fd180b549434b7598b716b877f747b3b26215b4c002a8f22ff3e392a0ff25685aa57654ff0d12cbdb8806a4261d8f263_1280.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="About.html" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Contact.html" style="padding: 10px 20px;">Contact</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.html">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="About.html">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Contact.html">Contact</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-clearfix u-section-1" id="carousel_6d75">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <img class="u-expanded-width u-image u-image-default u-image-1" src="images/71c4cede5fbbd8b7269b9eba06cc2c5dde1e8f436c1aff602cc5bf307064c997f68f87755c568d1192e0888c8cce6b5e13c9eb39b574aa7e9125bc_1280.jpg" alt="" data-image-width="1280" data-image-height="853">
+        <div class="u-container-style u-group u-white u-group-1">
+          <div class="u-container-layout u-container-layout-1">
+            <h4 class="u-custom-font u-font-montserrat u-text u-text-1"> Get started&nbsp;</h4>
+            <div class="u-expanded-width u-form u-form-1">
+              <form action="sendmail.php" method="POST" class="u-clearfix u-form-spacing-15 u-form-vertical u-inner-form"  style="padding: 0px;">
+                <div class="u-form-group u-form-name">
+                  <label for="name-4c18" class="u-label">First Name</label>
+                  <input type="text" placeholder="Enter your Name" id="name-4c18" name="firstName" class="u-border-2 u-border-grey-5 u-grey-5 u-input u-input-rectangle u-radius-10" >
+                </div>
+                <div class="u-form-group u-form-name">
+                  <label for="name-4c18" class="u-label">Last Name</label>
+                  <input type="text" placeholder="Enter your Name" id="name-4c18" name="lastName" class="u-border-2 u-border-grey-5 u-grey-5 u-input u-input-rectangle u-radius-10" >
+                </div>
+                <div class="u-form-email u-form-group">
+                  <label for="email-4c18" class="u-label">Email</label>
+                  <input type="email" placeholder="Enter a valid email address" id="email-4c18" name="email" class="u-border-2 u-border-grey-5 u-grey-5 u-input u-input-rectangle u-radius-10" >
+                </div>
+                <div class="u-form-group u-form-message">
+                  <label for="message-4c18" class="u-label">Message</label>
+                  <textarea placeholder="Enter your message" rows="4" cols="50" id="message-4c18" name="message" class="u-border-2 u-border-grey-5 u-grey-5 u-input u-input-rectangle u-radius-10" ></textarea>
+                </div>
+               
+                <div class="u-align-right u-form-group u-form-submit">
+                  <button type="submit" class="u-active-palette-2-light-1 u-border-active-palette-4-light-1 u-border-hover-palette-4-light-1 u-border-none u-btn u-btn-round u-btn-submit u-button-style u-hover-palette-2-light-1 u-palette-2-base u-radius-10 u-btn-1">Submit</button>
+                </div>
+                <!-- <div class="u-form-send-message u-form-send-success"> Thank you! Your message has been sent. </div>
+                <div class="u-form-send-error u-form-send-message"> Unable to send your message. Please fix errors then try again. </div> -->
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-image u-shading u-section-2" src="" data-image-width="1280" data-image-height="853" id="sec-a41b">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <h1 class="u-text u-text-default u-title u-text-1"> Lorem ipsum dolor.</h1>
+        <p class="u-large-text u-text u-text-default u-text-variant u-text-2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et gravida risus, non vestibulum turpis. Mauris non finibus lorem, in imperdiet sem. Nunc eget neque quis lectus placerat aliquet sit amet sit amet nulla. Aliquam vitae lectus laoreet, fringilla neque quis, vehicula est. Vivamus sit amet risus eu tortor scelerisque egestas a id eros. Integer eget odio nibh. Nunc non tempor lectus. Praesent vitae volutpat leo. In hac habitasse platea dictumst. Etiam luctus turpis eu blandit volutpat. Aliquam quis molestie lacus, ut varius enim.</p>
+        <a href="#" class="u-btn u-button-style u-palette-2-base u-btn-1">Read More</a>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-3" id="sec-f9f4">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout" style="">
+            <div class="u-layout-row" style="">
+              <div class="u-align-left u-container-style u-image u-layout-cell u-left-cell u-shading u-size-30 u-size-xs-60 u-image-1" src="" data-image-width="1280" data-image-height="853">
+                <div class="u-container-layout u-valign-middle u-container-layout-1" src="">
+                  <h2 class="u-text u-text-default u-text-1">Sample Headline</h2>
+                  <p class="u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+                  <a href="#" class="u-btn u-button-style u-palette-1-base u-btn-1">Button</a>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-grey-5 u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-layout-cell-2">
+                <div class="u-container-layout u-valign-middle u-container-layout-2">
+                  <h2 class="u-text u-text-default u-text-3">Sample Headline</h2>
+                  <p class="u-text u-text-4">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+                  <a href="#" class="u-btn u-button-style u-palette-1-base u-btn-2">Button</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-19d2"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus.</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section><span style="height: 64px; width: 64px; margin-left: 0px; margin-right: auto; margin-top: 0px; background-image: none; right: 20px; bottom: 20px; padding: 15px;" class="u-back-to-top u-icon u-icon-circle u-opacity u-opacity-85 u-palette-1-base" data-href="#">
+        <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 551.13 551.13"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-1d98"></use></svg>
+        <svg class="u-svg-content" enable-background="new 0 0 551.13 551.13" viewBox="0 0 551.13 551.13" xmlns="http://www.w3.org/2000/svg" id="svg-1d98"><path d="m275.565 189.451 223.897 223.897h51.668l-275.565-275.565-275.565 275.565h51.668z"></path></svg>
+    </span>
+  
+</body></html>
